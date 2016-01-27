fcd90099898888936f8ff71f85aa067d5fd656e0
Alexandre Frigout <alexandre.frigout@gmail.com>
2016-01-27 11:24:40 +0100

    Update regbackend.py

--- network/regbackend.py
+++ network/regbackend.py
@@ -2,16 +2,16 @@ from registration.backends.default.views import RegistrationView
 
 
 class ProfileBackend(RegistrationView):
-	
-	def register(self,request, **kwargs):
-		from network.models import Profile
-		user = super(ProfileBackend, self).register(request, **kwargs)
-		user.save()
-		profile = Profile.objects.create(
-									user=user, gender=kwargs['gender'])
-		profile.save()
 
-	def get_form_class(self, request):
+        def register(self,request, **kwargs):
+                from network.models import Profile
+                user = super(ProfileBackend, self).register(request, **kwargs)
+                user.save()
+                profile = Profile.objects.create(user=user, gender=kwargs['gender'])
+                profile.save()
 
-		from network.form import UserRegistrationForm
-		return UserRegistrationForm
+        def get_form_class(self, request):
+
+                from network.form import UserRegistrationForm
+                return UserRegistrationForm
+
